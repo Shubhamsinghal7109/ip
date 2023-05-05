29fca25fd22de38afee744766bda3efe32ac1644
Shubhamsinghal7109 <96278790+Shubhamsinghal7109@users.noreply.github.com>
2023-05-05 14:36:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import requests
 import urllib.request, urllib.parse, urllib.error
 import json
 import datetime
-ip=request.remote_addr 
+import requests
+ip=requests.get('http://checkip.amazonaws.com').text.rstrip()
 serviceurl = 'http://www.geoplugin.net/json.gp?ip='+ip
 uh = urllib.request.urlopen(serviceurl)
 data = uh.read().decode()
